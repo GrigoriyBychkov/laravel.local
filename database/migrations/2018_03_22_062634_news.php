84f698fafd15e8b5652be7d002946d6562815a06
Grigoriy <imansiberia@icloud.com>
2018-03-22 14:31:34 +0600

    create news model (correct db name news)

--- database/migrations/2018_03_22_062634_news.php
+++ database/migrations/2018_03_22_062634_news.php
@@ -13,7 +13,7 @@ class News extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('author_id')->unsigned()->default(0);
             $table->foreign('author_id')
