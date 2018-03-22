8cafd297ac0e036a66a6cca663af8dc31722cad3
Grigoriy <imansiberia@icloud.com>
2018-03-22 13:11:41 +0600

    db posts

---
+++ database/migrations/2018_03_22_062634_news.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class News extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('author_id')->unsigned()->default(0);
+            $table->foreign('author_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->string('title')->unique();
+            $table->text('body');
+            $table->string('img')->unique();
+            $table->string('file')->unique();
+            $table->boolean('active');
+            $table->timestamps();
+   });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
